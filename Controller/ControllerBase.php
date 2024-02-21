761d47da8819d227ce1a6bec2a4ac7113a31ea16
jk <jan.kukorowski@wp.pl>
2024-02-21 20:56:46 +0100

    getting posted json

--- Controller/ControllerBase.php
+++ Controller/ControllerBase.php
@@ -5,6 +5,7 @@ use MF\Config\Config;
 
 abstract class ControllerBase{
     protected \PDO $db;
+    protected array|null $postData;
     public function __construct(){
         try {
             $this->db = new \PDO("mysql:host=".Config::config['host'].";dbname=".Config::config['dbName'], Config::config['dbUser'], Config::config['dbPass']);
@@ -13,5 +14,8 @@ abstract class ControllerBase{
             echo json_encode(['error' => 'internal server error']);
             die();
         }
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST')
+            $this->postData = json_decode(file_get_contents('php://input'), true);
     }
 }
