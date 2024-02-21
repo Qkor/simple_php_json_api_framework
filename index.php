55b53ebd51f42c44e7bd495ad80990b9e9c1958a
jk <jan.kukorowski@wp.pl>
2024-02-21 20:18:23 +0100

    route validation

--- index.php
+++ index.php
@@ -14,13 +14,18 @@ header('Content-Type: application/json');
 
 $url = parse_url($_SERVER['REQUEST_URI']);
 $path = array_slice(explode('/',$url['path']),3);
-$controllerName = "\\MF\\Controller\\" . ucfirst($path[0]);
+$controllerName = ucfirst($path[0]);
 $function = $path[1];
 
 try {
-    $controller = new $controllerName();
+    if(!ctype_alnum($controllerName) || !ctype_alnum($function))
+        throw new Exception();
+    $fullControllerName = "\\MF\\Controller\\" . ucfirst($path[0]);
+    $controller = new $fullControllerName();
     $response = $controller->$function();
-    if(is_array($response)) echo json_encode($response);
+    if(is_array($response)){
+        echo json_encode($response);
+    }
     else{
         http_response_code(500);
         echo json_encode(['error' => 'internal server error']);
