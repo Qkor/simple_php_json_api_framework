0cdcbe0343a04558dbc4cceeb2ebba4d527b7dff
jk <jan.kukorowski@wp.pl>
2024-02-22 19:31:37 +0100

    added get params to ControllerBase and moved error response to separate function

--- Controller/ControllerBase.php
+++ Controller/ControllerBase.php
@@ -5,17 +5,20 @@ use MF\Config\Config;
 
 abstract class ControllerBase{
     protected \PDO $db;
-    protected array|null $postData;
+    protected array|null $input;
+    protected array|null $params;
     public function __construct(){
         try {
             $this->db = new \PDO("mysql:host=".Config::config['host'].";dbname=".Config::config['dbName'], Config::config['dbUser'], Config::config['dbPass']);
         } catch (\Exception $_) {
-            http_response_code(500);
-            echo json_encode(['error' => 'internal server error']);
-            die();
+            $this->errorResponse(500, 'Internal server error');
         }
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-            $this->postData = json_decode(file_get_contents('php://input'), true);
+        $this->input = json_decode(file_get_contents('php://input'), true);
+        $this->params = $_GET;
+    }
+    protected function errorResponse(int $httpCode, string $errorMessage): void{
+        http_response_code($httpCode);
+        echo json_encode(['error' => $errorMessage]);
+        die();
     }
 }
