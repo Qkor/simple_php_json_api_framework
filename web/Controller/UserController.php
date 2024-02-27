4ddcdc94683f1d9187a658ab5f2b10e8ec02c5f3
Jan Kukorowski <jan.kukorowski@vao.pl>
2024-02-27 14:03:02 +0100

    creating user session

--- web/Controller/UserController.php
+++ web/Controller/UserController.php
@@ -19,8 +19,18 @@ class UserController extends ControllerBase {
         $password = password_hash($this->input['password'], PASSWORD_DEFAULT);
         if($this->userService->getUserByUsername($username))
             return $this->errorResponse(1,'user with this username already exists');
-        if($this->userService->addUser($username, $password))
-            return ['success' => true];
+        $this->db->beginTransaction();
+        if($uid = $this->userService->addUser($username, $password)){
+            if($token = $this->userService->createUserSession($uid)){
+                $this->db->commit();
+                return [
+                    'uid' => $uid,
+                    'username' => $username,
+                    'token' => $token
+                ];
+            }
+        }
+        $this->db->rollBack();
         return $this->errorResponse(1, 'could not create a user');
     }
 
--- web/Service/UserService.php
+++ web/Service/UserService.php
@@ -6,17 +6,35 @@ use Qkor\Entity\User;
 
 class UserService extends ServiceBase {
     public function addUser($username, $password) : false|string {
-        $query = $this->db->prepare("INSERT INTO user (username, password) VALUES (?, ?)");
-        if($query->execute([$username,$password]))
-            return $this->db->lastInsertId();
+        $query = $this->db->prepare("INSERT INTO user (username, password, created, updated) VALUES (?, ?, ?, ?)");
+        if($query->execute([$username, $password, time(), time()]))
+            return (int)$this->db->lastInsertId();
         return false;
     }
+
+    public function getUser($uid) : false|User {
+        $query = $this->db->prepare("SELECT id, username FROM user WHERE id=?");
+        if($query->execute([$uid])){
+            $query->setFetchMode(\PDO::FETCH_CLASS, 'Qkor\Entity\User');
+            return $query->fetch();
+        }
+        return false;
+    }
+
     public function getUserByUsername($username) : false|User {
         $query = $this->db->prepare("SELECT id, username FROM user WHERE username=?");
         if($query->execute([$username])){
             $query->setFetchMode(\PDO::FETCH_CLASS, 'Qkor\Entity\User');
             return $query->fetch();
         }
+        return false;
+    }
+
+    public function createUserSession($uid){
+        $token = bin2hex(openssl_random_pseudo_bytes(32));
+        $query = $this->db->prepare("INSERT INTO session (token, uid, expires, created, updated) VALUES (?, ?, ?, ?, ?)");
+        if($query->execute([$token, $uid, time()+3600000, time(), time()]))
+            return $token;
         return false;
     }
 }
