a9f77ce3bbe1d315e7a2362fefe9b7923cdd1dac
Jan Kukorowski <jan.kukorowski@vao.pl>
2024-02-27 12:54:44 +0100

    simplified routing

--- web/index.php
+++ web/index.php
@@ -17,31 +17,28 @@ header('Content-Type: application/json');
 
 $url = parse_url($_SERVER['REQUEST_URI']);
 $path = array_slice(explode('/',$url['path']),Config::config['urlPathOffset']);
-if(count($path)>=2){
-    $controllerName = ucfirst($path[0]);
-    $route = $path[1];
-    if(ctype_alnum($controllerName) && ctype_alnum($route)){
-        $fullControllerName = "\\Qkor\\Controller\\" . ucfirst($path[0]) . "Controller";
-        if(class_exists($fullControllerName)){
-            $controller = new $fullControllerName();
-            $method = $controller->getRoute($route);
+if(count($path)>=2 && ctype_alnum($path[0]) && ctype_alnum($path[1])){
+    $controllerName = "\\Qkor\\Controller\\" . ucfirst($path[0]) . "Controller";
+    if(class_exists($controllerName)){
+        try{
+            $controller = new $controllerName();
+            $method = $controller->getRoute($path[1]);
             if(!$method && Config::config['autoRoutes'])
-                $method = $route;
+                $method = $path[1];
             if(method_exists($controller, $method)){
-                try {
-                    $response = $controller->$method();
-                    echo json_encode($response);
-                } catch (Throwable $e) {
-                    if($e->getCode() == 1){
-                        echo json_encode(ErrorHandler::getErrorResponse(1, $e->getMessage()));
-                    } else {
-                        if(Config::config['debug'])
-                            echo $e->getMessage();
-                        echo json_encode(ErrorHandler::getServerErrorResponse());
-                    }
-                }
+                $response = $controller->$method();
+                echo json_encode($response);
                 die();
             }
+        } catch (Throwable $e){
+            if($e->getCode() == 1){
+                echo json_encode(ErrorHandler::getErrorResponse(1, $e->getMessage()));
+            } else {
+                if(Config::config['debug'])
+                    echo $e->getMessage();
+                echo json_encode(ErrorHandler::getServerErrorResponse());
+            }
+            die();
         }
     }
 }
